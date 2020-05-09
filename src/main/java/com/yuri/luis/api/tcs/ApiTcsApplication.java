2a1cc9c5e532590da72ae1d6476d5c8a81f439b2
@yuri_luis_garciia_pereira <yuri.pereira@hbsis.com.br>
2020-05-09 16:52:35 -0300

    Projeto Criado Spring boot

---
+++ src/main/java/com/yuri/luis/api/tcs/ApiTcsApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.luis.api.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/luis/api/tcs/ApiTcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.luis.api.tcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
